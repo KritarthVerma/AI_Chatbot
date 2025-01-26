08001058428afd6885ccea830236d21d9fefc00a
Kritarth Verma <kritarth4444@gmail.com>
2025-01-26 21:05:34 +0530

    Created Signup Route

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { config } from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import morgan from 'morgan';
+import appRouter from './routes/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const envPath = path.resolve(__dirname, '../../.env'); // Move up two levels
@@ -9,5 +11,8 @@ config({ path: envPath }); //.env configuration
 const app = express();
 //---------------------------MIDDLEWARES------------------------
 app.use(express.json()); //parses JSON payloads into JavaScript objects, making them accessible via req.body
+app.use(morgan("dev")); //middleware for logging HTTP requests in Node.js.
+//"dev" is a predefined logging format that outputs concise colored logs with method, status, response time, and size
+app.use("/api/v1", appRouter);
 export default app;
 //# sourceMappingURL=app.js.map
---
+++ Backend/dist/controllers/userControllers.js
@@ -0,0 +1,26 @@
+import userModel from "../models/userModel.js";
+import { hash } from 'bcrypt';
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await userModel.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+export const userSignUp = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new userModel({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(201).json({ message: "OK", id: user._id.toString() });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "ERROR", cause: error.message });
+    }
+};
+//# sourceMappingURL=userControllers.js.map
